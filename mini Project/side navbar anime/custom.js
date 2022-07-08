17d182f24c2ddc4e4ce726071265780d938987a6
Nima Noorali <96994243+mynameisNN2@users.noreply.github.com>
2022-07-08 13:59:41 +0430

    Add files via upload

---
+++ mini Project/side navbar anime/custom.js
@@ -0,0 +1,8 @@
+let spanElem = document.querySelector("span");
+let SideBarElem = document.getElementById("sidebarnav");
+function openNavBar() {
+  SideBarElem.style.width = "150px";
+}
+function closeSidebar() {
+  SideBarElem.style.width = "0px";
+}
